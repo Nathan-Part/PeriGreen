f941a3ba2227c45a40c09d54657b4fcec1740711
Massongy <jacquesvollet@yahoo.fr>
2026-03-26 17:37:57 +0100

    modification de apis.ts pour la variable api url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Token management
 const getToken = (): string | null => localStorage.getItem('token');
